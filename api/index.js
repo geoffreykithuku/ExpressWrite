90cae5d50f9dd121f065134aaf26a9146be35878
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2023-07-29 03:15:06 +0300

    added edit button

--- api/index.js
+++ api/index.js
@@ -134,9 +134,23 @@ app.get("/posts", async (req, res) => {
     console.error(e);
   }
 });
+
 app.get("/posts/:id", async (req, res) => {
   const { id } = req.params;
-  const post = await Post.findById(id).populate("author", ["username"]);
 
-  res.json(post);
+  try {
+    const post = await Post.findById(id).populate("author", [
+      "username",
+      "_id",
+    ]);
+
+    if (!post) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+
+    res.json(post);
+  } catch (error) {
+    console.error("Error fetching post:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
