01651e08969487c997b0221dff26e8f687ee1d10
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2023-07-30 09:58:38 +0300

    update post

--- api/index.js
+++ api/index.js
@@ -154,3 +154,36 @@ app.get("/posts/:id", async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 });
+
+
+app.patch("/post", uploadMiddleware.single("file"), async (req, res) => {
+  let newPath = null
+  if (req.file) {
+    const { originalname, path } = req.file;
+    const parts = originalname.split(".");
+    const ext = parts[parts.length - 1];
+
+    newPath = path + "." + ext;
+    fs.renameSync(path, newPath);
+  }
+  
+
+  const { token } = req.cookies;
+
+  jwt.verify(token, secret, {}, async (err, info) => {
+    if (err) throw err;
+    const { id, title, content } = req.body;
+    
+    const postDoc = await Post.findById(id)
+    const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+    if (!isAuthor) {
+      res.status(400).json('You are not the author')
+      
+}
+   postDoc.updateOne({title, content, cover: newPath? newPath : postDoc.cover})
+
+   
+
+    res.status(200).json(postDoc);
+  });
+});
